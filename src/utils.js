9eccfbbc9aa57db4369d05a6f5a8336955478b46
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-05-05 09:37:46 +0300

    first test pass

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
 // Your code here
+ export function isPalindrome(word){
+    console.log(word)
+    let length =word.length
+    const newWordArray=[]
+    for (let i =word.length;i > 0;i--){
+    newWordArray.push(word.charAt(length-=1) )
+    }
+    console.log(newWordArray.join(''))  
+    if(newWordArray.join('')=== word){
+    return true
+   }else{
+    return false
+   }
+}
